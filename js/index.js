c0083dab774e3a6ac55d7fbcdc942a7a06ce2e51
MG <mgespirituuu27@gmail.com>
2024-05-18 08:46:00 -0700

    new file:   css/footer.css 	new file:   css/global.css 	new file:   css/header.css 	modified:   css/index.css 	new file:   css/resume.css 	modified:   footer.html 	modified:   header.html 	modified:   index.html 	modified:   js/index.js 	new file:   package-lock.json 	new file:   package.json 	modified:   resume.html

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-function loadResume() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onload = function () {
-        if (this.status === 200) {
-            document.getElementById("resume").innerHTML = this.responseText;
-        } else {
-            console.error("Error loading resume");
-        }
-    };
-    xhttp.open("GET", "resume.html", true);
-    xhttp.send();
+function toggleResume() {
+    const resumeDiv = document.getElementById("resume");
+
+    if (resumeDiv.innerHTML === "") {
+        // Load the resume content
+        const xhttp = new XMLHttpRequest();
+        xhttp.onload = function () {
+            if (this.status === 200) {
+                resumeDiv.innerHTML = this.responseText;
+            } else {
+                console.error("Error loading resume");
+            }
+        };
+        xhttp.open("GET", "resume.html", true);
+        xhttp.send();
+    } else {
+        // Clear the resume content
+        resumeDiv.innerHTML = "";
+    }
 }
